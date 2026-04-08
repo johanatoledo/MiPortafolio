cc12e524d42605cf444695c951f2479f84d0808f
johanatoledo <johanatoledo1990@gmail.com>
2026-04-08 15:48:02 -0500

    Refactor:Cambio orden secciones componentes

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Navbar from './components/Navbar';
 import Hero from './pages/Hero';
 import AboutMe from './pages/AboutMe';
 import Projects from './components/Projects';
-import FaQuestion from './components/FaQ';
+import Faq from './components/Faq';
 import Services from './components/Services';
 import Skills from './components/Skills';
 import Blog from './pages/Blog';
@@ -16,21 +16,23 @@ export default function App() {
   return (
     <DarkModeProvider>
       <BrowserRouter>
-     
         <div className="min-h-screen font-sans transition-colors duration-300 bg-white dark:diagonal-gradient-pro">
           <Navbar />
           <section id="home"> <Hero /> </section>
           <section id="about"><AboutMe /></section>
-         
+          
+        
+          <section id="services"><Services /></section>
+
+          <section id="proj"><Projects /></section>
+          <section id="faq"><Faq /></section>
+          <section id="contact"><Contact /></section>
+
           <Routes>
             <Route path="/" element={null} /> 
-            <Route path='/Services' element={<Services />} />
-            <Route path='/Contact' element={<Contact />} />
             <Route path='/blog' element={<Blog />} />
-            <Route path="*" element={null} />
           </Routes>
-          <section id="proj"><Projects /></section>
-          <section id="faq"><FaQuestion /></section>
+          
           <Footer />
         </div>
       </BrowserRouter>
