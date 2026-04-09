0ed91408406bffc4cdada20065cf1f768dc34564
johanatoledo <johanatoledo1990@gmail.com>
2026-04-08 22:53:27 -0500

    Refactor:Estilo flotante

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,15 @@ export default {
       fontFamily: {
         sans: ['Inter', 'sans-serif'],
       },
+      keyframes: {
+        float: {
+          '0%, 100%': { transform: 'translateY(0px)' },
+          '50%': { transform: 'translateY(-20px)' },
+        },
+      },
+      animation: {
+        float: 'float 4s ease-in-out infinite',
+      },
     },
   },
   plugins: [],
